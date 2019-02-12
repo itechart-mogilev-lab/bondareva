ca092a4d564ef46804674f015ce717be9b0ab57a
Viktoria <vika.bondareva.2014@mail.ru>
2019-02-13 00:43:54 +0300

    webpack setup

---
+++ task3/src/index.js
@@ -0,0 +1,10 @@
+import {Controller} from './controller';
+
+let controller =  new Controller(5);
+console.log(controller.getValue());
+
+controller.exponentiation(3);
+console.log(controller.getValue());
+
+
+
