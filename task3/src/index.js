64db20303b54a8ff02020b74ae93bcc914766d5e
Viktoria <vika.bondareva.2014@mail.ru>
2019-02-13 22:29:57 +0300

    done task 3

--- task3/src/index.js
+++ task3/src/index.js
@@ -1,8 +1,33 @@
 import { Controller } from "./controller";
+import "./style.css";
+
+let inputClass = "input_disable";
 
 let controller = new Controller(5);
 console.log(controller.getValue());
 
+let div = document.createElement("div");
+div.classList = ["container"];
+
+let input = document.createElement("input");
+input.classList.add("input");
+input.classList.add(inputClass);
+
+let btn = document.createElement("button");
+btn.innerText = "Send";
+btn.classList = ["button"];
+
+btn.addEventListener("click", () => {
+  if (input.classList.contains(inputClass)) {
+    input.classList.remove(inputClass);
+  } else {
+    input.classList.add(inputClass);
+  }
+});
+
+document.body.appendChild(div);
+div.appendChild(input);
+div.appendChild(btn);
+
 controller.exponentiation(3);
 console.log(controller.getValue());
-
