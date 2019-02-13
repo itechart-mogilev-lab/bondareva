1dd65c7d8fd4734a4183759e306021404cf50fbf
Viktoria <vika.bondareva.2014@mail.ru>
2019-02-13 09:56:14 +0300

    add field dist in gitignore

--- task3/src/index.js
+++ task3/src/index.js
@@ -5,6 +5,4 @@ console.log(controller.getValue());
 
 controller.exponentiation(3);
 console.log(controller.getValue());
-// "@babel/core": "^7.2.2",
-//     "@babel/preset-env": "^7.3.1",
-//     "@babel/preset-stage-2": "^7.0.0",
+
